d9e4810668d8d2ee4e50ec7741f4fffe0bf64a6e
hyggenygge <ketul@hafnet.dk>
2020-12-09 18:44:16 +0100

    NewJob and AddAnExpense added.

---
+++ ModelWebApi/src/components/AddAnExpense.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="columns">
+        <div class="column is-two-thirds">
+            <section class="section">
+                <h1 class="title">Add An Expense</h1>
+                <p class="subtitle">
+                    View health as an investment, not an <strong>expense</strong>!
+                </p>
+                <hr>
+            </section>
+
+            <section class="form">
+                <form v-on:submit.prevent="onSubmitForm">
+                    <div class="field">
+                        <label class="label">JobId</label>
+                        <div class="control">
+                            <input name="EfJobId" v-model="form.efJobId" class="input" type="number" min="0">
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label class="label">Expense</label>
+                        <div class="control">
+                            <input name="Expense" v-model="form.expense" class="input" type="number" min="0">
+                        </div>
+                    </div>
+
+                    <p></p>
+                    <div class="field-is-grouped">
+                        <div class="control">
+                            <button class="button is-primary">
+                                Add
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </section>
+        </div>
+</template>
+
+
+<script>
+    export default {
+        name: "VNTForm",
+        data: () => ({
+            form: {
+                efJoblId: "",
+                expense: ""
+            }
+        }),
+        methods: {
+            onSubmitForm() {
+                var url = "https://localhost:44368/api/Jobs";
+                fetch(url, {
+                    method: 'POST',
+                    body: JSON.stringify(this.form),
+                    credentials: 'include',
+                    headers: {
+                        'Authorization': 'Bearer ' + localStorage.getItem("token"),
+                        'Content-Type': 'application/json'
+                    }
+                }).then(responseJson => {
+                    this.response = responseJson;
+                })
+                    .catch(error => alert('Something bad happened: ' + error));
+
+            }
+        }
+    }
+</script>
--- ModelWebApi/src/components/NewJob.vue
+++ ModelWebApi/src/components/NewJob.vue
@@ -1,21 +1,112 @@
 <template>
-    <section class="form">
-        <form v-on:submit.prevent="onSubmitForm">
-            <div class="field">
-                <label class="label">Name</label>
-                <div class="control">
-                    <input name="name" v-model="form.name" v-validate="'required|min:3'" v-bind:class="{'is-danger': errors.has('name')}" class="input" type="text" placeholder="Full name">
-                </div>
-                <p class="help is-danger" v-show="errors.has('name')">
-                    {{ errors.first('name') }}
+    <div class="columns">
+        <div class="column is-two-thirds">
+            <section class="section">
+                <h1 class="title">Create New Job</h1>
+                <p class="subtitle">
+                    Work work work work <strong>work</strong>!
                 </p>
-            </div>
-        </form>
-    </section>
+                <hr>
+            </section>
+
+            <section class="form">
+                <form v-on:submit.prevent="onSubmitForm">
+                    <div class="field">
+                        <label class="label">Customer</label>
+                        <div class="control">
+                            <input name="customer"
+                                   v-model="form.customer"
+                                   v-validate="'required|min:3'"
+                                   class="input" type="text" placeholder="Customer">
+                        </div>
+                        <!--<p class="help is-danger" v-show="errors.has('customer')">
+           {{ errors.first('customer') }}
+        </p>-->
+                    </div>
+
+
+                    <!-- MISSING STARTDATE -->
+
+
+                    <div class="field">
+                        <label class="label">Days</label>
+                        <div class="control">
+                            <input firstname="days"
+                                   v-model="form.days"
+                                   v-validate="'required|min:3'"
+                                   class="input" type="number" placeholder="Days" min="0">
+                        </div>
+                        <!--<p class="help is-danger" v-show="errors.has('days')">
+            {{ errors.first('days') }}
+        </p>-->
+
+
+                    </div>
+
+                    <div class="field">
+                        <label class="label">Location</label>
+                        <div class="control">
+                            <input firstname="location"
+                                   v-model="form.location"
+                                   v-validate="'required|min:3'"
+                                   class="input" type="text" placeholder="Location">
+                        </div>
+                        <!--<p class="help is-danger" v-show="errors.has('location')">
+            {{ errors.first('location') }}
+        </p>-->
+                    </div>
+
+                    <div class="field">
+                        <label class="label">Comments</label>
+                        <div class="control">
+                            <textarea class="textarea" placeholder="Textarea" v-model="form.comments"></textarea>
+                        </div>
+                    </div>
+                    <div class="field is-grouped">
+                        <div class="control">
+                            <button class="button is-primary">
+                                Submit
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </section>
+
+        </div>
+    </div>
 
 </template>
 
 <script>
+    export default {
+        name: "NewModelForm",
+        data: () => ({
+            form: {
+                Customer: "",
+                Days: "",
+                Location: "",
+                Comments: ""
+            }
+        }),
+        methods: {
+            onSubmitForm() {
+                var url = "https://localhost:44368/api/Jobs";
+                fetch(url, {
+                    method: 'POST',
+                    body: JSON.stringify(this.form),
+                    credentials: 'include',
+                    headers: {
+                        'Authorization': 'Bearer ' + localStorage.getItem("token"),
+                        'Content-Type': 'application/json'
+                    }
+                }).then(responseJson => {
+                    this.response = responseJson;
+                })
+                    .catch(error => alert('Something bad happened: ' + error));
+
+            }
+        }
+    }
 </script>
 
 <style scoped>
